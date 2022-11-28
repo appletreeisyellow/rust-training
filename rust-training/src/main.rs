92f7696621dc8a68e156ebc866aacf2daa243da8
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-11-28 13:28:36 -0600

    chore: update

--- rust-training/src/main.rs
+++ rust-training/src/main.rs
@@ -34,6 +34,65 @@ fn vectors() {
     println!("scores: {scores:?}");
 }
 
+fn iterators() {
+    for i in (0..10).filter(|y| y % 2 == 0).map(|x| x * 2) {
+        print!("{i} ");
+    }
+    println!("");
+}
+
+fn iterators_single_value() {
+    let values: Vec<i32> = (0..10).collect();
+    let total: i32 = (0..10).sum();
+    println!("collect {values:?}");
+    println!("total {total}");
+}
+
+fn exercise_filter_sum() {
+    let mut sum = 0;
+    for i in (0..100).filter(|x| x % 3 == 0).filter(|x| x % 7 == 0) {
+        println!("{i}");
+        sum += i;
+    }
+    println!("sum {sum}");
+}
+
+#[derive(Debug)] // This will allow you to print a value with {:?}
+struct Fahrenheit {
+    value: f32,
+}
+
+#[derive(Debug)]
+struct Celsius {
+    value: f32,
+}
+
+fn f_to_c(f: Fahrenheit) -> Celsius {
+    Celsius {
+        value: (f.value - 32.0) / 1.8,
+    }
+}
+
+impl Celsius {
+    fn to_f(&self) -> Fahrenheit {
+        Fahrenheit {
+            value: self.value * 1.8 + 32.0,
+        }
+    }
+}
+
+fn fizzbuzz(n: i32) -> String {
+    if n % 3 == 0 && n % 5 == 0 {
+        "FizzBuzz".to_string()
+    } else if n % 3 == 0 {
+        "Fizz".to_string()
+    } else if n % 5 == 0 {
+        "Buzz".to_string()
+    } else {
+        n.to_string()
+    }
+}
+
 fn main() {
     print_values();
 
@@ -43,4 +102,22 @@ fn main() {
     println!("fib2(10)={}", fibonacci2(10));
 
     vectors();
+
+    iterators();
+
+    iterators_single_value();
+
+    exercise_filter_sum();
+
+    println!(
+        "100.0F is {:.1}C",
+        f_to_c(Fahrenheit { value: 100.0 }).value
+    );
+
+    let c = Celsius { value: 10.0 };
+    println!("{:.1}C is {:.1}F", c.value, c.to_f().value);
+
+    for i in 1..=100 {
+        println!("{}", fizzbuzz(i));
+    }
 }
